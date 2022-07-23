378b2bc128b08039ceec7d5c47456ab6daa9d40c
&! Tekky#1337 <98614666+xtekky@users.noreply.github.com>
2022-07-23 18:27:47 +0200

    old stuff

--- v1/main.py
+++ v1/main.py
@@ -1,6 +1,6 @@
 #-------------------------------------------------------------------------------------------------
 # X-Reporter by Tekky
-# Copyright (c) 2022 Tekky. All rights reserved.
+# Copyright (c) 2022 Tekky. All rights reserved. 
 # I am not responsible for any damage caused by your use of this software.
 #-------------------------------------------------------------------------------------------------
 
